8281f7695def87c68111b5fef7dae956bb3b1e0f
GerardoGran <gerardogr.al@outlook.com>
2022-06-15 01:51:47 -0600

    follow

--- util/simplify_productions.py
+++ util/simplify_productions.py
@@ -78,7 +78,7 @@ def remove_unit_productions(grammar: dict, non_terminals: set):
 def get_first_sets(grammar: dict, non_terminals: set) -> dict:
     """
     Gets first sets for each non-terminal symbol.
-    Goes in revers order from keys to ensure other symbols have their first() sets complete.
+    Goes in reverse order from keys to ensure other symbols have their first() sets complete.
 
     args
         grammar: previously built grammar dictionary
@@ -87,35 +87,31 @@ def get_first_sets(grammar: dict, non_terminals: set) -> dict:
     returns
         first_sets: dictionary of sets with each symbol's first set
     """
-
-    first_sets = {}  # Dictionary with first sets
+    first_sets = dict()  # Dictionary with first sets
 
     pending = set()
     # Iterate through keys in reverse order
     for symbol in list(grammar.keys())[::-1]:
         current_set = set()
         for production in grammar[symbol]:
-            print(f"{symbol}\t->\t{production}")
             first_symbol = production[0]
             if first_symbol in non_terminals:
                 if first_symbol in first_sets.keys():
-                    current_set.union(first_sets[first_symbol])
+                    current_set = current_set.union(first_sets[first_symbol])
                 else:
                     pending.add(symbol)
             else:
                 current_set.add(first_symbol)
+
         first_sets[symbol] = current_set
 
-    print(pending)
     for symbol in pending:
-        pending.remove(symbol)
         current_set = set()
         for production in grammar[symbol]:
-            print(f"{symbol}\t->\t{production}")
             first_symbol = production[0]
             if first_symbol in non_terminals:
-                if first_symbol in first_sets.values():
-                    current_set.union(first_sets[symbol])
+                if first_symbol in first_sets.keys():
+                    current_set = current_set.union(first_sets[first_symbol])
                 else:
                     pending.add(symbol)
             else:
@@ -125,7 +121,67 @@ def get_first_sets(grammar: dict, non_terminals: set) -> dict:
     return first_sets
 
 
+def get_follow_sets(grammar: dict, non_terminals: set, first_sets: set) -> dict:
+    """
+    Gets follow sets for each non-terminal symbol.
+
+    args
+        grammar: previously built grammar dictionary
+        non_terminals: set of all non-terminals in grammar
+        first_sets: dictionary of sets with each symbol's first set
+
+    returns
+        first_sets: dictionary of sets with each symbol's follow set
+    """
+    follow = dict()
+
+    # initialize empty follow sets
+    for symbol in non_terminals:
+        follow[symbol] = set()
+
+    # run through grammar 5 times to ensure all follows are accounted for
+    for n in range(5):
+        for i, key in enumerate(list(grammar.keys())):
+            if i == 0:
+                follow[key].add("$")
+            for production in grammar[key]:
+                for j, symbol in enumerate(production):
+                    if symbol in non_terminals:
+                        if j == len(production) - 1:
+                            follow[symbol] = follow[symbol].union(follow[key])
+                            break
+                        for follow_symbol in production[j+1:]:
+                            ended_eps = False
+                            if follow_symbol not in non_terminals:
+                                follow[symbol].add(follow_symbol)
+                                break
+                            elif "ε" in first_sets[follow_symbol]:
+                                follow[symbol] = follow[symbol].union(
+                                    first_sets[follow_symbol])
+                                ended_eps = True
+                            else:
+                                follow[symbol] = follow[symbol].union(
+                                    first_sets[follow_symbol])
+                                break
+                        if ended_eps:
+                            follow[symbol] = follow[symbol].union(follow[key])
+
+    # remove epsilons
+    for symbol in follow:
+        if "ε" in follow[symbol]:
+            follow[symbol].remove("ε")
+
+    return follow
+
+
+def show_sets(type: str, sets: dict, grammar: dict):
+    for symbol in grammar:
+        print(f"{type}({symbol}) = {{{', '.join(sets[symbol])}}}")
+
+
 grammar, non_terminals = get_grammar_from_txt("producciones.txt")
 first_sets = get_first_sets(grammar, non_terminals)
+follow_sets = get_follow_sets(grammar, non_terminals, first_sets)
 
-print(first_sets)
+# show_sets("FIRST", first_sets, grammar)
+show_sets("FOLLOW", follow_sets, grammar)
